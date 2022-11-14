69a95fab0093e2ae1940697c5a53212efd66dea8
MatthieuWerbrouck <matthieu@werbrouck.ch>
2022-11-15 00:03:02 +0100

    Test selecteur

--- assets/js/questionnaire.js
+++ assets/js/questionnaire.js
@@ -1,11 +1,26 @@
 const IPADRESS = 'http://localhost:3000/'
-
+let httpRequest = new XMLHttpRequest();
+//sendRequest("users");
+init()
+// httpRequest.open('GET', IPADRESS + "users", true);
+// console.log(IPADRESS + "users")
+// httpRequest.responseType = 'json';
+// httpRequest.send();
 
 
 httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         let rep = httpRequest.response;
         console.log(rep);
+        let select = document.getElementById("SelectEleves")
+        for (let index = 0; index < rep.length; index++) {
+            opt = document.createElement("option")
+            opt.text = rep[index].name
+            opt.value = rep[index].id
+                
+            select.add(opt,null)            
+        }
+        
     } else {
         console.log("ALERTE")
 }
@@ -18,8 +33,9 @@ function init() {
 
 
 function sendRequest(route) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', IPADRESS & route, true);
+
+    httpRequest.open('GET', IPADRESS + route, true);
     httpRequest.responseType = 'json';
     httpRequest.send();
+    
 }
