f96fb98133425b2f2d13656d369fb31a68fc0253
MatthieuWerbrouck <matthieu@werbrouck.ch>
2022-11-22 01:36:44 +0100

    test fetch

---
+++ assets/js/questionnaire_old.js
@@ -0,0 +1,51 @@
+const IPADRESS = 'http://localhost:3000/'
+let httpRequest = new XMLHttpRequest();
+
+// const instance = axios.create({
+//     baseURL: APIADRESS,
+//     timeout: 1000,
+//     headers: {'X-Custom-Header': 'foobar'}
+//   });
+
+
+// axios.get('users')
+// .then(function(rep){
+//     console.log(rep.data)
+// })
+// console.log("ok")
+
+
+
+
+
+
+// axios({
+//     method: 'get',
+//     url: IPADRESS + "users",
+//     responseType: 'JSON'
+// })
+// .then(function(rep){
+//     console.log(rep)
+// })
+
+
+// fetch(IPADRESS + "questions")
+// .then(function(rep){
+//     return rep.json()
+// })
+// .then(function(json){
+//     console.log(json)
+//     let divQuestions = document.getElementById("Questionnaire")
+//     for (let i = 0; i < json.length; i++) {
+//         let elem = document.createElement("div")
+//         elem.innerHTML = json[i].lib
+        
+//         divQuestions.appendChild(elem)
+        
+//     }
+// })
+
+console.log("OK")
+
+
+
