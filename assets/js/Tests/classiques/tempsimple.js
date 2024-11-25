7ed1ecf00345544d0d07d02998dc03af0f7b3648
MatthieuWerbrouck <matthieu@werbrouck.ch>
2024-11-25 20:30:21 +0100

    Debut structure tableau terminaison ( reflechir passe simple ( plusieurs faux groupes))

---
+++ assets/js/Tests/classiques/tempsimple.js
@@ -0,0 +1,32 @@
+let terminaisons = [
+    "present" = [
+        ["e","es","e","ons","ez","ent"],
+        ["is","is","it","issons","issez","issent"],
+        ["s","s","t","ons","ez","ent"]
+    ],
+    "imparfait" = [
+        ["ais","ais","ait","ions","iez","aient"],
+        ["ais","ais","ait","ions","iez","aient"],
+        ["ais","ais","ait","ions","iez","aient"]
+    ],
+    "futur" = [
+        ["erai","eras","era","erons","erez","eront"],
+        ["irai","iras","ira","irons","irez","iront"],
+        ["rai","ras","ra","rons","rez","ront"]
+    ],
+    "passe" = [
+        ["","","","","",""],
+        ["","","","","",""],
+        ["","","","","",""]
+    ],
+    "conditionnel" = [
+        ["","","","","",""],
+        ["","","","","",""],
+        ["","","","","",""]
+    ],
+    "imperatif" = [
+        ["","","","","",""],
+        ["","","","","",""],
+        ["","","","","",""]
+    ]
+]
