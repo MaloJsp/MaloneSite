7803c3380861d016cf0f047d30fc6b69cdb2bcda
Malone <mallaury.fiedler@hotmail.com>
2024-12-15 17:28:44 +0100

    affichage

--- assets/js/Tests/classiques/displayer.js
+++ assets/js/Tests/classiques/displayer.js
@@ -15,7 +15,7 @@ function displayQuest(vb,rep,id,p){
     let mBal = getMainBal()
     let libPrsn = displayPrsn(p)
     mBal.innerHTML += `
-        <div class=".test">
+        <div class="test">
             <label for="${rep}">
               ${libPrsn} (${vb.vb}) 
             </label>
