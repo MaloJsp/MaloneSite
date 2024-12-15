8ac36e3a3b71aea54a2adc1a065df50f0a97dff4
Malone <mallaury.fiedler@hotmail.com>
2024-12-15 16:46:19 +0100

    Debut irrégulier

---
+++ assets/js/Tests/classiques/irreguliers/irreg.js
@@ -0,0 +1,509 @@
+[
+    {
+      "infinitif": "faire",
+      "conditionnel": [
+        "ferais",
+        "ferais",
+        "ferait",
+        "ferions",
+        "feriez",
+        "feraient"
+      ],
+      "futur": [
+        "ferai",
+        "feras",
+        "fera",
+        "ferons",
+        "ferez",
+        "feront"
+      ],
+      "imparfait": [
+        "faisais",
+        "faisais",
+        "faisait",
+        "faisions",
+        "faisiez",
+        "faisaient"
+      ],
+      "imperatif": [
+        "fais",
+        "faisons",
+        "faites"
+      ],
+      "passe_comp": [
+        "ai fait",
+        "as fait",
+        "a fait",
+        "avons fait",
+        "avez fait",
+        "ont fait"
+      ],
+      "passe": [
+        "fis",
+        "fis",
+        "fit",
+        "fîmes",
+        "fîtes",
+        "firent"
+      ],
+      "plus_que_parfait": [
+        "avais fait",
+        "avais fait",
+        "avait fait",
+        "avions fait",
+        "aviez fait",
+        "avaient fait"
+      ],
+      "present": [
+        "fais",
+        "fais",
+        "fait",
+        "faisons",
+        "faites",
+        "font"
+      ]
+    },
+    {
+      "infinitif": "aller",
+      "conditionnel": [
+        "irais",
+        "irais",
+        "irait",
+        "irions",
+        "iriez",
+        "iraient"
+      ],
+      "futur": [
+        "irai",
+        "iras",
+        "ira",
+        "irons",
+        "irez",
+        "iront"
+      ],
+      "imparfait": [
+        "allais",
+        "allais",
+        "allait",
+        "allions",
+        "alliez",
+        "allaient"
+      ],
+      "imperatif": [
+        "va",
+        "allons",
+        "allez"
+      ],
+      "passe_comp": [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      "passe": [
+        "allai",
+        "allas",
+        "alla",
+        "allâmes",
+        "allâtes",
+        "allèrent"
+      ],
+      "plus_que_parfait": [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      "present": [
+        "vais",
+        "vas",
+        "va",
+        "allons",
+        "allez",
+        "vont"
+      ]
+    },
+    {
+      "infinitif": "dire",
+      "conditionnel": [
+        "dirais",
+        "dirais",
+        "dirait",
+        "dirions",
+        "diriez",
+        "diraient"
+      ],
+      "futur": [
+        "dirai",
+        "diras",
+        "dira",
+        "dirons",
+        "direz",
+        "diront"
+      ],
+      "imparfait": [
+        "disais",
+        "disais",
+        "disait",
+        "disions",
+        "disiez",
+        "disaient"
+      ],
+      "imperatif": [
+        "dis",
+        "disons",
+        "dites"
+      ],
+      "passe_comp": [
+        "ai dit",
+        "as dit",
+        "a dit",
+        "avons dit",
+        "avez dit",
+        "ont dit"
+      ],
+      "passe": [
+        "dis",
+        "dis",
+        "dit",
+        "dîmes",
+        "dîtes",
+        "dirent"
+      ],
+      "plus_que_parfait": [
+        "avais dit",
+        "avais dit",
+        "avait dit",
+        "avons dit",
+        "avez dit",
+        "avaient dit"
+      ],
+      "present": [
+        "dis",
+        "dis",
+        "dit",
+        "disons",
+        "dites",
+        "disent"
+      ]
+    },
+    {
+      "infinitif": "venir",
+      "conditionnel": [
+        "viendrais",
+        "viendrais",
+        "viendrait",
+        "viendrions",
+        "viendriez",
+        "viendraient"
+      ],
+      "futur": [
+        "viendrai",
+        "viendras",
+        "viendra",
+        "viendrons",
+        "viendrez",
+        "viendront"
+      ],
+      "imparfait": [
+        "venais",
+        "venais",
+        "venait",
+        "venions",
+        "veniez",
+        "venaient"
+      ],
+      "imperatif": [
+        "viens",
+        "venons",
+        "venez"
+      ],
+      "passe_comp": [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      "passe": [
+        "vins",
+        "vins",
+        "vint",
+        "vînmes",
+        "vîntes",
+        "vinrent"
+      ],
+      "plus_que_parfait": [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      "present": [
+        "viens",
+        "viens",
+        "vient",
+        "venons",
+        "venez",
+        "viennent"
+      ]
+    },
+    {
+      "infinitif": "pouvoir",
+      "conditionnel": [
+        "pourrais",
+        "pourrais",
+        "pourrait",
+        "pourrions",
+        "pourriez",
+        "pourraient"
+      ],
+      "futur": [
+        "pourrai",
+        "pourras",
+        "pourra",
+        "pourrons",
+        "pourrez",
+        "pourront"
+      ],
+      "imparfait": [
+        "pouvais",
+        "pouvais",
+        "pouvait",
+        "pouvions",
+        "pouviez",
+        "pouvaient"
+      ],
+      "passe_comp": [
+        "ai pu",
+        "as pu",
+        "a pu",
+        "avons pu",
+        "avez pu",
+        "ont pu"
+      ],
+      "passe": [
+        "pus",
+        "pus",
+        "put",
+        "pûmes",
+        "pûtes",
+        "purent"
+      ],
+      "plus_que_parfait": [
+        "avais pu",
+        "avais pu",
+        "avait pu",
+        "avions pu",
+        "aviez pu",
+        "avaient pu"
+      ],
+      "present": [
+        "peux",
+        "peux",
+        "peut",
+        "pouvons",
+        "pouvez",
+        "peuvent"
+      ]
+    },
+    {
+      "infinitif": "voir",
+      "conditionnel": [
+        "verrais",
+        "verrais",
+        "verrait",
+        "verrions",
+        "verriez",
+        "verraient"
+      ],
+      "futur": [
+        "verrai",
+        "verras",
+        "verra",
+        "verrons",
+        "verrez",
+        "verront"
+      ],
+      "imparfait": [
+        "voyais",
+        "voyais",
+        "voyait",
+        "voyions",
+        "voyiez",
+        "voyaient"
+      ],
+      "imperatif": [
+        "vois",
+        "voyons",
+        "voyez"
+      ],
+      "passe_comp": [
+        "ai vu",
+        "as vu",
+        "a vu",
+        "avons vu",
+        "avez vu",
+        "ont vu"
+      ],
+      "passe": [
+        "vis",
+        "vis",
+        "vit",
+        "vîmes",
+        "vîtes",
+        "virent"
+      ],
+      "plus_que_parfait": [
+        "avais vu",
+        "avais vu",
+        "avait vu",
+        "avions vu",
+        "aviez vu",
+        "avaient vu"
+      ],
+      "present": [
+        "vois",
+        "vois",
+        "voit",
+        "voyons",
+        "voyez",
+        "voient"
+      ]
+    },
+    {
+      "infinitif": "vouloir",
+      "conditionnel": [
+        "voudrais",
+        "voudrais",
+        "voudrait",
+        "voudrions",
+        "voudriez",
+        "voudraient"
+      ],
+      "futur": [
+        "voudrai",
+        "voudras",
+        "voudra",
+        "voudrons",
+        "voudrez",
+        "voudront"
+      ],
+      "imparfait": [
+        "voulais",
+        "voulais",
+        "voulait",
+        "voulions",
+        "vouliez",
+        "voulaient"
+      ],
+      "imperatif": [
+        "veuille",
+        "veuillons",
+        "veuillez"
+      ],
+      "passe_comp": [
+        "ai voulu",
+        "as voulu",
+        "a voulu",
+        "avons voulu",
+        "avez voulu",
+        "ont voulu"
+      ],
+      "passe": [
+        "voulus",
+        "voulus",
+        "voulut",
+        "voulûmes",
+        "voulûtes",
+        "voulurent"
+      ],
+      "plus_que_parfait": [
+        "avais voulu",
+        "avais voulu",
+        "avait voulu",
+        "avions voulu",
+        "aviez voulu",
+        "avaient voulu"
+      ],
+      "present": [
+        "veux",
+        "veux",
+        "veut",
+        "voulons",
+        "voulez",
+        "veulent"
+      ]
+    },
+    {
+      "infinitif": "prendre",
+      "conditionnel": [
+        "prendrais",
+        "prendrais",
+        "prendrait",
+        "prendrions",
+        "prendriez",
+        "prendraient"
+      ],
+      "futur": [
+        "prendrai",
+        "prendras",
+        "prendra",
+        "prendrons",
+        "prendrez",
+        "prendront"
+      ],
+      "imparfait": [
+        "prenais",
+        "prenais",
+        "prenait",
+        "prenions",
+        "preniez",
+        "prenaient"
+      ],
+      "imperatif": [
+        "prends",
+        "prenons",
+        "prenez"
+      ],
+      "passe_comp": [
+        "ai pris",
+        "as pris",
+        "a pris",
+        "avons pris",
+        "avez pris",
+        "ont pris"
+      ],
+      "passe": [
+        "pris",
+        "pris",
+        "prit",
+        "prîmes",
+        "prîtes",
+        "prirent"
+      ],
+      "plus_que_parfait": [
+        "avais pris",
+        "avais pris",
+        "avait pris",
+        "avions pris",
+        "aviez pris",
+        "avaient pris"
+      ],
+      "present": [
+        "prends",
+        "prends",
+        "prend",
+        "prenons",
+        "prenez",
+        "prennent"
+      ]
+    }
+]
